363f73f55c86758a6f0be3fbc4aac88e9ab948fb
andy0314 <andy20030314@gmail.com>
2023-01-01 23:02:16 +0800

    add course schema

---
+++ backend/src/models/course.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const CourseSchema = Schema({
+    course_id: String, //流水號
+    course_name: String, //課程名稱
+    course_id_: String, //識別碼
+    department: Array, //授課對象
+    course_code: String, //課號
+    class_id: String, //班次
+    teacher: String, //教師
+    half_year: Boolean, //全年
+    course_type: String, //必選修
+    credit: Number, //學分
+    method: String, //加簽
+    field: String, //領域專長
+    max_student: Number, //人數上限
+    time_place: String, //時間地點
+    limit: String, //修課限制
+    note: String, //備註
+})
+
+const Course = mongoose.model('course', CourseSchema);
+
+export default Course;
