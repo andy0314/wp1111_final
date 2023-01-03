23b0b4c40390fb211821516cadf819547caa4bcd
andy0314 <andy20030314@gmail.com>
2023-01-03 21:56:35 +0800

    remove datainit button

--- frontend/src/containers/HomePage.js
+++ frontend/src/containers/HomePage.js
@@ -1,14 +1,9 @@
 import api from "../api"
 
 const HomePage = () => {
-    const test = async () =>{
-        const { data: { messages, data } } = await api.get('/crawler/datainit');
-        console.log(messages);
-        console.log(data);
-    }
     return (
         <div>
-            <button onClick={test}>test</button>
+            <button>test</button>
         </div>
     )
 }
