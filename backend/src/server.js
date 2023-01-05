25e8279fb9f1677aa16f6d5bd8d1cec5292832ce
PattyWu <patty98261@gmail.com>
2023-01-05 15:15:33 +0800

    deploy to Railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,22 +5,37 @@ import Route from './routes'
 import "dotenv-defaults/config.js";
 
 import { dataInit, dataInitWithCrawler } from './init';
+const app = express();
+
+console.log("process.env.NODE_ENV: ", process.env.NODE_ENV)
+if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
+    app.use(express.json())
+    app.use('/api', Route);
+    const __dirname = path.resolve();
+    console.log("__dirname: ", __dirname);
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+ }
+else{
+    console.log("use cors")
+    app.use(cors());
+    app.use(express.json())
+    app.use('/api', Route);
+}
 
 const url = process.env.MONGO_URL
 const dboptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
-
 mongoose.set("strictQuery", true);
 
 mongoose.connect(url, dboptions).then((res) => {
     console.log("mongo db connection created");
     dataInit();
 })
-const app = express();
-app.use(cors());
-app.use(express.json())
-app.use('/api', Route);
+
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Server is up on port ${port}.`))
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+const API_ROOT =
+  process.env.NODE_ENV === "production"
+    ? "/api"
+    : "http://localhost:4000/api";
 
 const api = axios.create({
-  baseURL: `http://localhost:4000/api`,
+  baseURL: API_ROOT,
 });
 
 export default api;
